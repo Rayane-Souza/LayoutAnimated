4a8dbc94de9f1dfc78d8150c149fd39d7ae87a6a
Rayane Souza <130245094+Rayane-Souza@users.noreply.github.com>
2024-06-24 10:46:12 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import "./App.scss";
+import { ArrowIcon } from "./components/arrow-icon";
+import { Logo } from "./components/logo";
+
+function App() {
+  return (
+    <main>
+      <header className="header-container">
+        <Logo />
+        <nav className="menu-options">
+          <a>What we offer</a>
+          <a>How it works</a>
+          <a>Portifolio</a>
+        </nav>
+        <button>
+          <div className="btn-face-white">
+            Contact us
+            <ArrowIcon />
+          </div>
+          <div className="btn-face-purple">
+            Contact us
+            <ArrowIcon />
+          </div>
+        </button>
+      </header>
+      <div className="content-container">
+        <div className="heading-container">
+          <h2>Lets grow your</h2>
+          <h2>social presence.</h2>
+        </div>
+        <div className="bubble target">🎯</div>
+        <div className="bubble shop">🛍️</div>
+        <div className="bubble rocket">🚀</div>
+        <div className="bubble fire">🔥</div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
